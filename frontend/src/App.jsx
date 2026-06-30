26fb0af3525a567a4fe049a59f41def42cbc2a91
Roi <23-16160@g.batstate-u.edu.ph>
2026-06-30 10:22:38 +0800

    Core metrics calculation engine. KPI data parser and revenue aggregator utility.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import AnalyticsDashboard from './features/analytics/components/AnalyticsDashboard';
-
-function App() {
-  return (
-    <div>
-      <AnalyticsDashboard />
-    </div>
-  );
-}
-
 export default App;

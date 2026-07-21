d5f693525f30643d9e9b9bb744c58c4d0eabcb5f
RoiRendal <23-16160@g.batstate-u.edu.ph>
2026-07-21 17:09:01 +0800

    w5d2

--- frontend/src/__tests__/mockAPI.js
+++ frontend/src/__tests__/mockAPI.js
@@ -1,5 +1,5 @@
 import { beforeAll, afterEach, afterAll } from "vitest";
-import server from "./sample-backend/server";
+import { server } from "./sample-backend/server";
 
 beforeAll(() =>{
   server.listen();
